e191f61b18d171dc80687fac336e2edee33ca055
Wagner Chaga <wachag_7@hotmail.com>
2025-04-10 09:03:02 -0300

    feat: adcionando a função meu

---
+++ menu.py
@@ -0,0 +1,22 @@
+# 1.3 Implemente uma função chamada menu que ofereça as seguintes opções ao usuário:
+# 1. Somar
+# 2. Subtrair
+# 3. Multiplicar
+# 4 . Dividir
+# 0. Sair
+
+
+#
+
+
+def menu():
+    while True:
+
+
+print("Escolha uma opção:")
+print(menu.somar(4, 2))
+print(menu.subtracao(4 - 2))
+print(menu.multiplicar(3 * 2))
+print(menu.divisao(10 / 2))
+
+opcao = input("Digite uma opção:")
